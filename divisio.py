262cee45e5e165c261870ca5e98e88318af74895
rgasulla <rgasulla@instituticaria.cat>
2024-10-10 09:49:18 +0200

    divisio

---
+++ divisio.py
@@ -0,0 +1,6 @@
+print('Anem a fer una divisió amb nombres enters')
+D = int(input('Quin és el dividend de la divisió? '))
+d = int(input('I el divisor? '))
+print('Divisió: ', D,'/',d)
+print('Quocient: ', D//d)
+print('Residu: ', D%d)
